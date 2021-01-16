b324749eb9105a3dd9f9857967b8a2b5309a1974
leocassiosilva <leocassiosilva1234@gmail.com>
2021-01-16 18:36:15 -0300

    multiplos componentes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Bomdia from './componentes/BomDia'
-
+import{BoaTarde, BoaNoite} from './componentes/Multiplos'
 //const elemento = <h1>React</h1>
-ReactDOM.render(<Bomdia nome="Leocassio"/>, document.getElementById('root'))
+ReactDOM.render(
+<div>
+    <Bomdia nome="Leocassio"/>
+    <BoaTarde nome="Leocassio"/>
+    <BoaNoite nome="Leocassio"/>
+</div>
+    
+, document.getElementById('root'))
 
